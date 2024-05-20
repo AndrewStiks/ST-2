18e09d9360ea3ec18296bebbc06a55b29e470b75
unknown <slipfis@gmail.com>
2024-05-20 07:22:17 +0300

    initial

---
+++ include/tasks.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Derun Andrey
+
+#pragma once
+
+double SolveEarthAndRope(double earthRadius, double ropelength);
+double SolveSwimmingPool(double poolRadiusMeter, double areaWidthMeter,
+                         double areaPricePerSquareMeter,
+                         double ferencePricePerMeter);
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,46 @@
-// Copyright 2022 UNN-CS
-#include <cstdint>
+// Copyright 2024 Derun Andrey
 #include "circle.h"
 
+#include <cmath>
+#include <cstdint>
+#include <stdexcept>
+
+
+double const Circle::PI = std::atan(1.0) * 4;
+
+double Circle::CheckingTheInputData(double value) {
+  if (value < 0) {
+    throw std::invalid_argument("The value must be greater than zero");
+  }
+  return value;
+}
+
+Circle::Circle(double radiusValue) {
+  radius = CheckingTheInputData(radiusValue);
+  ference = 2 * PI * radius;
+  area = PI * radius * radius;
+}
+
+double Circle::GetRadius() const { return radius; }
+
+double Circle::GetFerence() const { return ference; }
+
+double Circle::GetArea() const { return area; }
+
+void Circle::SetRadius(double value) {
+  radius = CheckingTheInputData(value);
+  ference = radius * 2 * PI;
+  area = radius * radius * PI;
+}
+
+void Circle::SetFerence(double value) {
+  ference = CheckingTheInputData(value);
+  radius = ference / (2 * PI);
+  area = radius * radius * PI;
+}
+
+void Circle::SetArea(double value) {
+  area = CheckingTheInputData(value);
+  radius = std::sqrt(area / PI);
+  ference = radius * 2 * PI;
+}
